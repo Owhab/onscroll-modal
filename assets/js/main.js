06ffbf70c8ee4ccd6990e549135959221eb1f417
Md. Abdul Owhab <owhab.cse@gmail.com>
2022-12-05 11:31:07 +0600

    added popup modal with 200px scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,17 @@ window.addEventListener('scroll', ()=>{
     const scrolled = window.scrollY;
     console.log(scrolled)
     if(scrolled == 200){
-        alert("Hey, Hello World")
+        // alert("Hey, Hello World")
+        document.querySelector('.popup').classList.add('active');
     }
 
+})
+
+/* document.querySelector("#show-login").addEventListener('click', function(){
+    document.querySelector('.popup').classList.add('.active');
+    
+}) */
+document.querySelector(".popup .close-btn").addEventListener('click', function(){
+    document.querySelector('.popup').classList.remove('active');
+    
 })
