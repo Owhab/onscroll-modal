6ac2985a8457f5b8ccbff407fab93e9134e897a8
Md. Abdul Owhab <owhab.cse@gmail.com>
2022-12-05 18:28:52 +0600

    added reset on sumit the form

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,9 +20,9 @@ document.querySelector(".popup .close-btn").addEventListener('click', function()
 
 
 // Reset the form
-document.querySelector('.form-element .submit-btn').addEventListener('click', (e)=>{
+document.querySelector('.form').addEventListener('submit', (e)=>{
     e.preventDefault();
-    // console.log("Submited");
+    console.log("Submited");
     document.querySelector('#name').value = "";
     document.querySelector('#email').value = "";
     document.querySelector('#tel').value = "";
